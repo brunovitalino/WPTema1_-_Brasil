eb17afa868a44db8ee10fef1ccdd677701bb570e
Bruno <Bruno@BrunoNB.localdomain>
2017-05-06 11:46:20 -0300

    Implementado a área de Fale Conosco.
    Foi instalado o plugin Formidable Forms para auxiliar nessa tarefa. Os
    labels foram escondidos. E os inputs, textareas, e buttons alterados.
    
    Signed-off-by: Bruno <Bruno@BrunoNB.localdomain>

--- bvbrasil/footer.php
+++ bvbrasil/footer.php
@@ -1,4 +1,13 @@
 
+<div class="footer">
+	<div class="wrap">
+		<h2>Fale conosco</h2>
+		<?php 
+			echo FrmFormsController::get_form_shortcode( array('id' => 2, 'key' => '', 'title' => false, 'description' => false, 'readonly' => false, 'entry_id' => false) );
+		?>
+	</div>
+</div>
+
 <script type="text/javascript">
 	$("[data-fancybox]").fancybox({
 		// Options will go here
